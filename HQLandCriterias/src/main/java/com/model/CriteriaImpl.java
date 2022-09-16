8e216508d12c27e6677827438e88fab5e8feca12
kokanevina <kokane.vina@gmail.com>
2022-09-16 12:30:08 +0530

    Hibernate Criteria

--- HQLandCriterias/src/main/java/com/model/CriteriaImpl.java
+++ HQLandCriterias/src/main/java/com/model/CriteriaImpl.java
@@ -7,6 +7,7 @@ import org.hibernate.Criteria;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
+import org.hibernate.criterion.Projections;
 import org.hibernate.criterion.Restrictions;
 
 import com.db.MyConnection;
@@ -27,7 +28,9 @@ public class CriteriaImpl {
 	//	cr.add(Restrictions.gt("empSalary", 35000.0));
 		//cr.add(Restrictions.ge("empSalary", 35000.0));
 		//cr.add(Restrictions.lt("empSalary", 35000.0));
-		cr.add(Restrictions.le("empSalary", 35000.0));
+		//cr.add(Restrictions.le("empSalary", 35000.0));
+		//cr.add(Restrictions.ne("empSalary", 35000.0));
+		cr.add(Restrictions.eq("empSalary", 35000.0));
 		empList= cr.list();
 		tr.commit();
 		}
@@ -36,4 +39,22 @@ public class CriteriaImpl {
 		}
 		empList.forEach(System.out::println);
 	}
+	
+	public void criteriaMeth2(){
+		List mylist=null;
+		try(Session session=sfactory.openSession();){
+		Transaction tr=session.beginTransaction();
+		Criteria cr=session.createCriteria(Employee.class);
+		//cr.add(Restrictions.eq("empSalary", 35000.0));
+		cr.add(Restrictions.eq("qualification", "ME"));
+		cr.setProjection(Projections.count("empId"));
+		mylist=cr.list();
+		tr.commit();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println(mylist);
+	}
+	
 }
--- HQLandCriterias/src/main/java/com/test/CriteriaTest.java
+++ HQLandCriterias/src/main/java/com/test/CriteriaTest.java
@@ -5,6 +5,6 @@ import com.model.CriteriaImpl;
 public class CriteriaTest {
 	public static void main(String[] args) {
 		CriteriaImpl criteriaimpl=new CriteriaImpl();
-		criteriaimpl.criteriaMeth1();
+		criteriaimpl.criteriaMeth2();
 	}
 }
